0f0f04d951aa7606268b9badbe875b1733ac34f6
cjmakes <cj@cjmakes.com>
2023-12-28 00:01:48 -0800

    cleanup

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # test1
+
+This is a demo of a public repo that can clone a private repo and do things
+with it.
--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-eval `ssh-agent -s`
-ssh-add -L
-echo ssh-add - <<< $TEST2_KEY
-ssh-add -L
-
-git clone ssh://git@github.com/cjmakes/test2
-cat test2/README.md
