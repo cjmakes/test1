23107eabbc1c0aa721da8e46a9288faddb3e6e1e
cjmakes <cj@cjmakes.com>
2023-12-28 00:10:37 -0800

    custom action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-eval `ssh-agent -s`
-ssh-add -L
-echo ssh-add - <<< $TEST2_KEY
-ssh-add -L
-
+mkdir -p ~/.ssh
+echo $1 > ~/.ssh/id_ed25519
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 git clone ssh://git@github.com/cjmakes/test2
-cat test2/README.md
+cat test2/README.md > $GITHUB_OUTPUT
